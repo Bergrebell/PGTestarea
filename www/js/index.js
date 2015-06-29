dbeba0c14a5f8ba663d65ec1a6e3c0718a0e8d8e
Roman Kuepper <rrkuepper@gmail.com>
2015-06-29 12:03:50 +0200

    Cross site requests whitelisted in cordova

--- www/js/index.js
+++ www/js/index.js
@@ -17,24 +17,64 @@ $(document).ready(function() {
         var form = $(this);
 
         //Alert for debug
-        alert(form.data('clicked'));
+        //alert(form.data('clicked'));
 
         $.ajax({
             url: form.attr('action'),
             type: form.attr("method"),
+            datatype: 'jsonp',
             data: { clickedButton : form.data('clicked') }
         });
     });
-
 });
 // *** End AJAX ***
 
+
+
 // *** Start API***
 
-document.addEventListener("deviceready", onDeviceReady, false);
+// Start Accelerometer
+// The watch id references the current `watchAcceleration`
+    var watchID = null;
+    // Wait for Cordova to load
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
+    // Cordova is ready
+    //
+    function onDeviceReady() {
+        startWatch();
+    }
+    // Start watching the acceleration
+    //
+    function startWatch() {
+        // Update acceleration every 3 seconds
+        var options = { frequency: 100 };
 
-function onDeviceReady() {
-    // Now safe to use device APIs
-}
+        watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+    }
+    // Stop watching the acceleration
+    //
+    function stopWatch() {
+        if (watchID) {
+            navigator.accelerometer.clearWatch(watchID);
+            watchID = null;
+        }
+    }
+
+    // onSuccess: Get a snapshot of the current acceleration
+    //
+    function onSuccess(acceleration) {
+        var element = document.getElementById('accelerometer');
+        element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
+                            'Acceleration Y: ' + acceleration.y + '<br />' +
+                            'Acceleration Z: ' + acceleration.z + '<br />' +
+                            'Timestamp: '      + acceleration.timestamp + '<br />';
+    }
+
+    // onError: Failed to get the acceleration
+    //
+    function onError() {
+        alert('onError!');
+    }
 
 // *** End API***
